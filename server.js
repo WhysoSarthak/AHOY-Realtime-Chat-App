1b7a85a5ef057dfc8837f791e99fad58bf087808
WhysoSarthak <sarthakchoudhary507@gmail.com>
2022-01-19 02:16:31 +0530

    commit

--- server.js
+++ server.js
@@ -13,3 +13,14 @@ app.use(express.static(__dirname + '/public'))
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
 })
+
+// Socket 
+const io = require('socket.io')(http)
+
+io.on('connection', (socket) => {
+    console.log('Connected...')
+    socket.on('message', (msg) => {
+        socket.broadcast.emit('message', msg)
+    })
+
+})
